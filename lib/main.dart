117e481b382f6d60ddc17c2b4ec71d63e6195e1a
Ghaith <ghayth.1900@gmail.com>
2025-07-11 11:18:20 +0200

    Re-initialize project and push again

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            "Hello World",
+            style: TextStyle(color: Colors.red),
+          ),
+        ),
+      ),
+    );
+  }
+}
