f06453a26960e1b9f98a88cc856f6e5d7d9f1cb5
Ghaith <ghayth.1900@gmail.com>
2025-07-13 13:09:10 +0200

    woring on local database

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:vpn_basic_project/preferences/app_preferences.dart';
 import 'package:vpn_basic_project/view/screens/home_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await AppPreferences.initHive();
   runApp(const MyApp());
 }
 
---
+++ lib/preferences/app_preferences.dart
@@ -0,0 +1,15 @@
+import 'package:hive_flutter/adapters.dart';
+
+class AppPreferences {
+  static late Box boxofdate;
+  static Future<void> initHive() async {
+    await Hive.initFlutter();
+    boxofdate = await Hive.openBox("data");
+  }
+
+  Future<void> setDarkMode(bool isDarkMode) async {
+    await boxofdate.put("isDarkMode", isDarkMode);
+  }
+
+  bool getDarkMode() => boxofdate.get("isDarkMode", defaultValue: false);
+}
