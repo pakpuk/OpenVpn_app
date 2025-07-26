7f041eee48ea3e668b97a937c181a540be194b1c
Ghaith <ghayth.1900@gmail.com>
2025-07-26 18:56:50 +0200

    start  working  on  vpn  rounded  btn

---
+++ lib/view/widgets/round_vpn.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RoundVpnbutton extends StatelessWidget {
+  const RoundVpnbutton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      radius: 50,
+    );
+  }
+}
