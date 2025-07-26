139a344012be28398598552e7550772b7484582b
Ghaith <ghayth.1900@gmail.com>
2025-07-26 19:41:35 +0200

    fix  some  errors

--- lib/view/widgets/round_vpn.dart
+++ lib/view/widgets/round_vpn.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:vpn_basic_project/theme/colors_manager.dart';
+import 'package:vpn_basic_project/theme/text_manger.dart';
 
 class RoundVpnbutton extends StatelessWidget {
   const RoundVpnbutton({super.key});
@@ -17,10 +18,21 @@ class RoundVpnbutton extends StatelessWidget {
             child: CircleAvatar(
               radius: 35,
               backgroundColor: ColorsManager.maincolor,
-              child: Icon(
-                Icons.power_settings_new,
-                color: ColorsManager.whiteColor,
-                size: 30,
+              child: Column(
+                children: [
+                  Icon(
+                    Icons.power_settings_new,
+                    color: ColorsManager.whiteColor,
+                    size: 20,
+                  ),
+                  SizedBox(height: 5),
+                  Text(TextManager.connecttxt,
+                      style: TextStyle(
+                        color: ColorsManager.whiteColor,
+                        fontSize: 12,
+                        fontWeight: FontWeight.w600,
+                      )),
+                ],
               ),
             ),
           ),
