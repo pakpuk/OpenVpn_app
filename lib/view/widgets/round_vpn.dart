b7683983e53a2be752d328eb7fbbe04bd6cc23b7
Ghaith <ghayth.1900@gmail.com>
2025-07-26 19:46:06 +0200

    fix  some  errors

--- lib/view/widgets/round_vpn.dart
+++ lib/view/widgets/round_vpn.dart
@@ -7,35 +7,38 @@ class RoundVpnbutton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CircleAvatar(
-        radius: 50,
-        backgroundColor: ColorsManager.redacclowop1,
-        child: CircleAvatar(
-          radius: 42,
-          backgroundColor: ColorsManager.redacclowop2,
-          child: InkWell(
-            onTap: () {},
-            child: CircleAvatar(
-              radius: 35,
-              backgroundColor: ColorsManager.maincolor,
-              child: Column(
-                children: [
-                  Icon(
-                    Icons.power_settings_new,
-                    color: ColorsManager.whiteColor,
-                    size: 20,
-                  ),
-                  SizedBox(height: 5),
-                  Text(TextManager.connecttxt,
-                      style: TextStyle(
-                        color: ColorsManager.whiteColor,
-                        fontSize: 12,
-                        fontWeight: FontWeight.w600,
-                      )),
-                ],
+    return Semantics(
+      button: true,
+      child: CircleAvatar(
+          radius: 50,
+          backgroundColor: ColorsManager.redacclowop1,
+          child: CircleAvatar(
+            radius: 42,
+            backgroundColor: ColorsManager.redacclowop2,
+            child: InkWell(
+              onTap: () {},
+              child: CircleAvatar(
+                radius: 35,
+                backgroundColor: ColorsManager.maincolor,
+                child: Column(
+                  children: [
+                    Icon(
+                      Icons.power_settings_new,
+                      color: ColorsManager.whiteColor,
+                      size: 20,
+                    ),
+                    SizedBox(height: 5),
+                    Text(TextManager.connecttxt,
+                        style: TextStyle(
+                          color: ColorsManager.whiteColor,
+                          fontSize: 12,
+                          fontWeight: FontWeight.w600,
+                        )),
+                  ],
+                ),
               ),
             ),
-          ),
-        ));
+          )),
+    );
   }
 }
