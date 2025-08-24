190313a98c330fe575239d718e4de73132c5e91b
Ghaith <ghayth.1900@gmail.com>
2025-08-24 14:28:53 +0200

    almost finihs working  on  vpn info model

--- lib/models/vpn_info.dart
+++ lib/models/vpn_info.dart
@@ -7,4 +7,15 @@ class VpnInfo {
   late final String countryShortName;
   late final int vpnSession;
   late final String base64OpenvpnConfigData;
+
+  VpnInfo({
+    required this.base64OpenvpnConfigData,
+    required this.vpnSession,
+    required this.countryShortName,
+    required this.countryLongName,
+    required this.speed,
+    required this.ping,
+    required this.ip,
+    required this.hostname,
+  });
 }
